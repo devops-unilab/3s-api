1f53082752a77ec446941259ab3e0066fb7ef1e2
Jefferson Uchôa Ponte <jefponte@gmail.com>
2023-06-17 12:04:13 -0300

    Start to separe html from controller in messages

--- source/app3s/controller/OcorrenciaController.php
+++ source/app3s/controller/OcorrenciaController.php
@@ -239,7 +239,178 @@ class OcorrenciaController
 		]);
 
 
-		$this->indexMessages($order);
+		// INDEX MESSAAGE
+		//DAqui pra baixo só usa $order
+		$canSendMessage = $this->possoEnviarMensagem($order);
+
+		$messageList = DB::table('mensagem_forum')
+		->join('usuario', 'mensagem_forum.id_usuario', '=', 'usuario.id')
+		->join('ocorrencia', 'mensagem_forum.id_ocorrencia', '=', 'ocorrencia.id')
+		->select(
+			'mensagem_forum.id as id',
+			'usuario.id as user_id',
+			'usuario.nome as user_name',
+			'mensagem_forum.tipo as message_type',
+			'mensagem_forum.mensagem as message_content',
+			'mensagem_forum.data_envio as created_at',
+			'ocorrencia.status as order_status'
+		)
+		->where('mensagem_forum.id_ocorrencia', $order->id)
+		->orderBy('mensagem_forum.id')
+		->get();
+
+        if (isset($_POST['chatDelete'])) {
+
+            $idChat = intval($_POST['chatDelete']);
+
+            $message = DB::table('mensagem_forum')
+            ->join('usuario', 'mensagem_forum.id_usuario', '=', 'usuario.id')
+            ->join('ocorrencia', 'mensagem_forum.id_ocorrencia', '=', 'ocorrencia.id')
+            ->select(
+                'mensagem_forum.id as id',
+                'usuario.id as user_id',
+                'ocorrencia.status as order_status'
+            )
+            ->where('mensagem_forum.id', $idChat)
+            ->first();
+
+            if ($sessao->getIdUsuario() === $message->user_id && $order->status === 'e') {
+
+                DB::table('mensagem_forum')->where('id', $idChat)->delete();
+                echo '<meta http-equiv = "refresh" content = "0 ; url =?page=ocorrencia&selecionar=' . $_GET['selecionar'] . '"/>';
+            }
+        }
+        echo '
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="modalDeleteChat" tabindex="-1" aria-labelledby="modalDeleteChatLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modalDeleteChatLabel">Apagar Mensagem</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                Tem certeza que deseja apagar esta mensagem?
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <form action="" method="post">
+                    <input type="hidden" id="chatDelete" name="chatDelete" value=""/>
+                    <button type="submit" class="btn btn-primary">Confirmar</button>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+		<div class="container">
+		<div class="row">
+			<div class="chatbox chatbox22">
+				<div class="chatbox__title">
+					<h5 class="text-white">#<span id="id-ocorrencia">' . $order->id . '</span></h5>
+					<!--<button class="chatbox__title__tray">
+            <span></span>
+        </button>-->
+
+				</div>
+				<div id="corpo-chat" class="chatbox__body">';
+
+
+
+
+        $ultimoId = 0;
+        foreach ($messageList as $mensagemForum) {
+            $ultimoId = $mensagemForum->id;
+            $name = $mensagemForum->user_name;
+            $name = substr(ucwords(mb_strtolower($name, 'UTF-8')), 0, 14).(strlen($name) > 14 ? "..." : "");
+
+            echo '
+
+
+
+            			<div class="chatbox__body__message chatbox__body__message--left">
+
+            				<div class="chatbox_timing">
+            					<ul>
+            						<li><a href="#"><i class="fa fa-calendar"></i> ' . date("d/m/Y", strtotime($mensagemForum->created_at)) . '</a></li>
+            						<li><a href="#"><i class="fa fa-clock-o"></i> ' . date("H:i", strtotime($mensagemForum->created_at)) . '</a></a></li>';
+            if ($mensagemForum->user_id == $sessao->getIdUsuario() && $mensagemForum->order_status === 'e') {
+                echo '<li><button data-toggle="modal" onclick="changeField(' . $mensagemForum->id . ')" data-target="#modalDeleteChat"><i class="fa fa-trash-o"></i> Apagar </a></button></li>';
+            }
+
+            echo '
+
+            					</ul>
+            				</div>
+            				<!-- <img src="https://www.gstatic.com/webp/gallery/2.jpg"
+            					alt="Picture">-->
+            				<div class="clearfix"></div>
+            				<div class="ul_section_full">
+            					<ul class="ul_msg">
+                                    <li><strong>' .$name . '</strong></li>';
+            if ($mensagemForum->message_type == self::TIPO_ARQUIVO) {
+                echo '<li>Anexo: <a href="uploads/' . $mensagemForum->message_content . '">Clique aqui</a></li>';
+            } else {
+                echo '
+                        <li>' . strip_tags($mensagemForum->message_content) . '</li>';
+            }
+            echo '
+
+            					</ul>
+            					<div class="clearfix"></div>
+
+            				</div>
+
+            			</div>';
+        }
+        echo '<span id="ultimo-id-post" class="escondido">' . $ultimoId . '</span>';
+        echo '
+
+
+				</div>
+				<div class="panel-footer">';
+        if ($canSendMessage) {
+            echo '<form id="insert_form_mensagem_forum" class="user" method="post">
+            <input type="hidden" name="enviar_mensagem_forum" value="1">
+            <input type="hidden" name="ocorrencia" value="' . $order->id . '">
+            <input type="hidden" id="campo_tipo" name="tipo" value="' . self::TIPO_TEXTO . '">
+
+            <div class="custom-control custom-switch">
+              <input type="checkbox" class="custom-control-input" name="muda-tipo" id="muda-tipo">
+              <label class="custom-control-label" for="muda-tipo">Enviar Arquivo</label>
+            </div>
+            <div class="custom-file mb-3 escondido" id="campo-anexo">
+                  <input type="file" class="custom-file-input" name="anexo" id="anexo" accept="application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint, text/plain, application/pdf, image/*, application/zip,application/rar, .ovpn, .xlsx">
+                  <label class="custom-file-label" for="anexo" data-browse="Anexar">Anexar um Arquivo</label>
+            </div>
+  <div class="input-group">
+    <input name="mensagem" id="campo-texto" type="text" class="form-control input-sm chat_set_height" placeholder="Digite sua mensagem aqui..." tabindex="0" dir="ltr" spellcheck="false" autocomplete="off" autocorrect="off" autocapitalize="off" contenteditable="true" />
+                <span class="input-group-btn"> <button class="btn bt_bg btn-sm" id="botao-enviar-mensagem">Enviar</button></span>
+  </div>
+        </form>';
+        }
+
+        echo '
+
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+    <script>
+    function changeField(id) {
+        document.getElementById(\'chatDelete\').value = id;
+    }
+    </script>
+
+';
 	}
 
 
@@ -1798,179 +1969,6 @@ class OcorrenciaController
         }
         return true;
     }
-
-    public function indexMessages($order)
-    {
-        $sessao = new Sessao();
-
-        if (isset($_POST['chatDelete'])) {
-
-            $idChat = intval($_POST['chatDelete']);
-
-            $message = DB::table('mensagem_forum')
-            ->join('usuario', 'mensagem_forum.id_usuario', '=', 'usuario.id')
-            ->join('ocorrencia', 'mensagem_forum.id_ocorrencia', '=', 'ocorrencia.id')
-            ->select(
-                'mensagem_forum.id as id',
-                'usuario.id as user_id',
-                'ocorrencia.status as order_status'
-            )
-            ->where('mensagem_forum.id', $idChat)
-            ->first();
-
-            if ($sessao->getIdUsuario() === $message->user_id && $order->status === 'e') {
-
-                DB::table('mensagem_forum')->where('id', $idChat)->delete();
-                echo '<meta http-equiv = "refresh" content = "0 ; url =?page=ocorrencia&selecionar=' . $_GET['selecionar'] . '"/>';
-            }
-        }
-        echo '
-
-
-        <!-- Modal -->
-        <div class="modal fade" id="modalDeleteChat" tabindex="-1" aria-labelledby="modalDeleteChatLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="modalDeleteChatLabel">Apagar Mensagem</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                Tem certeza que deseja apagar esta mensagem?
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                <form action="" method="post">
-                    <input type="hidden" id="chatDelete" name="chatDelete" value=""/>
-                    <button type="submit" class="btn btn-primary">Confirmar</button>
-                </form>
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-<div class="container">
-		<div class="row">
-			<div class="chatbox chatbox22">
-				<div class="chatbox__title">
-					<h5 class="text-white">#<span id="id-ocorrencia">' . $order->id . '</span></h5>
-					<!--<button class="chatbox__title__tray">
-            <span></span>
-        </button>-->
-
-				</div>
-				<div id="corpo-chat" class="chatbox__body">';
-
-            $messageList = DB::table('mensagem_forum')
-            ->join('usuario', 'mensagem_forum.id_usuario', '=', 'usuario.id')
-            ->join('ocorrencia', 'mensagem_forum.id_ocorrencia', '=', 'ocorrencia.id')
-            ->select(
-                'mensagem_forum.id as id',
-                'usuario.id as user_id',
-                'usuario.nome as user_name',
-                'mensagem_forum.tipo as message_type',
-                'mensagem_forum.mensagem as message_content',
-                'mensagem_forum.data_envio as created_at',
-                'ocorrencia.status as order_status'
-            )
-            ->where('mensagem_forum.id_ocorrencia', $order->id)
-            ->orderBy('mensagem_forum.id')
-            ->get();
-
-
-        $ultimoId = 0;
-        foreach ($messageList as $mensagemForum) {
-            $ultimoId = $mensagemForum->id;
-            $name = $mensagemForum->user_name;
-            $name = substr(ucwords(mb_strtolower($name, 'UTF-8')), 0, 14).(strlen($name) > 14 ? "..." : "");
-
-            echo '
-
-
-
-            			<div class="chatbox__body__message chatbox__body__message--left">
-
-            				<div class="chatbox_timing">
-            					<ul>
-            						<li><a href="#"><i class="fa fa-calendar"></i> ' . date("d/m/Y", strtotime($mensagemForum->created_at)) . '</a></li>
-            						<li><a href="#"><i class="fa fa-clock-o"></i> ' . date("H:i", strtotime($mensagemForum->created_at)) . '</a></a></li>';
-            if ($mensagemForum->user_id == $sessao->getIdUsuario() && $mensagemForum->order_status === 'e') {
-                echo '<li><button data-toggle="modal" onclick="changeField(' . $mensagemForum->id . ')" data-target="#modalDeleteChat"><i class="fa fa-trash-o"></i> Apagar </a></button></li>';
-            }
-
-            echo '
-
-            					</ul>
-            				</div>
-            				<!-- <img src="https://www.gstatic.com/webp/gallery/2.jpg"
-            					alt="Picture">-->
-            				<div class="clearfix"></div>
-            				<div class="ul_section_full">
-            					<ul class="ul_msg">
-                                    <li><strong>' .$name . '</strong></li>';
-            if ($mensagemForum->message_type == self::TIPO_ARQUIVO) {
-                echo '<li>Anexo: <a href="uploads/' . $mensagemForum->message_content . '">Clique aqui</a></li>';
-            } else {
-                echo '
-                        <li>' . strip_tags($mensagemForum->message_content) . '</li>';
-            }
-            echo '
-
-            					</ul>
-            					<div class="clearfix"></div>
-
-            				</div>
-
-            			</div>';
-        }
-        echo '<span id="ultimo-id-post" class="escondido">' . $ultimoId . '</span>';
-        echo '
-
-
-				</div>
-				<div class="panel-footer">';
-        if ($this->possoEnviarMensagem($order)) {
-            echo '<form id="insert_form_mensagem_forum" class="user" method="post">
-            <input type="hidden" name="enviar_mensagem_forum" value="1">
-            <input type="hidden" name="ocorrencia" value="' . $order->id . '">
-            <input type="hidden" id="campo_tipo" name="tipo" value="' . self::TIPO_TEXTO . '">
-
-            <div class="custom-control custom-switch">
-              <input type="checkbox" class="custom-control-input" name="muda-tipo" id="muda-tipo">
-              <label class="custom-control-label" for="muda-tipo">Enviar Arquivo</label>
-            </div>
-            <div class="custom-file mb-3 escondido" id="campo-anexo">
-                  <input type="file" class="custom-file-input" name="anexo" id="anexo" accept="application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint, text/plain, application/pdf, image/*, application/zip,application/rar, .ovpn, .xlsx">
-                  <label class="custom-file-label" for="anexo" data-browse="Anexar">Anexar um Arquivo</label>
-            </div>
-  <div class="input-group">
-    <input name="mensagem" id="campo-texto" type="text" class="form-control input-sm chat_set_height" placeholder="Digite sua mensagem aqui..." tabindex="0" dir="ltr" spellcheck="false" autocomplete="off" autocorrect="off" autocapitalize="off" contenteditable="true" />
-                <span class="input-group-btn"> <button class="btn bt_bg btn-sm" id="botao-enviar-mensagem">Enviar</button></span>
-  </div>
-        </form>';
-        }
-
-        echo '
-
-
-
-				</div>
-			</div>
-		</div>
-	</div>
-    <script>
-    function changeField(id) {
-        document.getElementById(\'chatDelete\').value = id;
-    }
-    </script>
-
-';
-    }
-
 
 
     public function sendMailNotifyMessage(MensagemForum $mensagemForum, Ocorrencia $ocorrencia)
